e5c760bd4f89ac90010852ce163cf7ad1ae4b9aa
Romário Cabó <romariocabo2012@gmail.com>
2023-05-26 15:24:01 -0300

    first commit

---
+++ src/main/java/br/com/planilha/leitorapp/LeitorPlanilhasAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.planilha.leitorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitorPlanilhasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeitorPlanilhasAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/planilha/leitorapp/LeitorPlanilhasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.planilha.leitorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitorPlanilhasAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
